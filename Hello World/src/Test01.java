377ac31579f34bd1b648b275e83e816e1e4c555f
jongkyoung <jklee1865@naver.com>
2019-03-27 11:26:37 +0900

    sd

--- Hello World/src/Test01.java
+++ Hello World/src/Test01.java
@@ -4,10 +4,6 @@ public class Test01 {
 
 	public static void main(String[] args) {
 		System.out.println("¾È³ç");
-		Scanner sc = new Scanner("je");
-		int num = sc.nextInt();
-		System.out.println(num);
 		
 	}
-
 }
