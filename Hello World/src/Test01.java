7efb7fa2e659dfb60878cd8407106a6ab2e333be
jongkyoung <jklee1865@naver.com>
2019-03-27 10:25:07 +0900

    create project, create hello world

---
+++ Hello World/src/Test01.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Test01 {
+
+	public static void main(String[] args) {
+		System.out.println("¾È³ç");
+		Scanner sc = new Scanner("je");
+		int num = sc.nextInt();
+		System.out.println(num);
+		
+	}
+
+}
