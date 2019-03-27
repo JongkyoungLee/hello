8e7af8794d9e3ffd971cb78d547e7962010f860d
jongkyoung <jklee1865@naver.com>
2019-03-27 11:39:17 +0900

    commit

--- Hello World/src/Test01.java
+++ Hello World/src/Test01.java
@@ -5,6 +5,6 @@ public class Test01 {
 	public static void main(String[] args) {
 		System.out.println("¾È³ç");
 		System.out.println("World's End Dancehall");
-		
+		System.out.println("Earth's End Dancehall");
 	}
 }
