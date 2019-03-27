40d054a4958d3cd7ffba358c5b7d35b2aeba2414
jongkyoung <jklee1865@naver.com>
2019-03-27 11:20:57 +0900

    World's End Dancehall

--- Hello World/src/Test01.java
+++ Hello World/src/Test01.java
@@ -1,13 +1,12 @@
 import java.util.Scanner;
 
 public class Test01 {
-
+	
 	public static void main(String[] args) {
+	
 		System.out.println("¾È³ç");
-		Scanner sc = new Scanner("je");
-		int num = sc.nextInt();
-		System.out.println(num);
+		System.out.println("World's End Dancehall");
 		
 	}
-
+	
 }
