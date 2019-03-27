61a9277e8feb81375c5300b968370c19ff69ec16
jongkyoung <jklee1865@naver.com>
2019-03-27 11:44:03 +0900

    commit

--- Hello World/src/Test01.java
+++ Hello World/src/Test01.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class Test01 {
 	
 	public static void main(String[] args) {
-	
 		System.out.println("¾È³ç");
 		System.out.println("World's End Dancehall");
 		System.out.println("Hello Dance World");
