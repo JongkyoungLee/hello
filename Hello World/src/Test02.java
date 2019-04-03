eb809517bda294da38635ec4e8486c2ff6586a9c
JongkyoungLee <39550257+JongkyoungLee@users.noreply.github.com>
2019-04-03 10:44:52 +0900

    Update Test02.java
    everybody take it

--- Hello World/src/Test02.java
+++ Hello World/src/Test02.java
@@ -3,6 +3,7 @@ public class Test02 {
 		System.out.println("Add Java Class File Push");
 		System.out.println("World with Miku");
 		System.out.println("World with 42");
+		System.out.println("World Message");
 	}
 }
 
